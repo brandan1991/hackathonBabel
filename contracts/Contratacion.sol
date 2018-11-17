aa48d30d1bf8b6bf344d829c97d34e5c09b0f085
Francisco José Benito Moreno <jose.benito@babel-si.local>
2018-11-17 07:36:05 +0100

    jajaj

--- contracts/Contratacion.sol
+++ contracts/Contratacion.sol
@@ -18,4 +18,17 @@ contract Contratacion {
         clientes.push(CLiente(msg.sender, idServicio, msg.value));
         return idServicio;
     }
+
+
+    function devolver(uint idServicio) public payable  returns (uint) {
+        clientes.push(CLiente(msg.sender, idServicio, msg.value));
+
+       for (uint i = 0; i < clientes.length; i++) {
+            CLiente cliente = clientes[i];
+            if(cliente.addressCliente == msg.sender) {
+                msg.sender.transfer(cliente.importe);
+            }
+        }        
+        return idServicio;
+    }
 }
